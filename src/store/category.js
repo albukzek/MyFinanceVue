1c45281f4d0ee43a47fc82fc97fd59df855147df
albukzek <albukzek@gmail.com>
2021-02-19 00:16:33 +0300

    Add creator category

---
+++ src/store/category.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app"
+
+export default {
+  actions:{
+    async createCategory({commit, dispatch},{title,limit}){
+      try{
+        const uid = await dispatch('getUid')
+        console.log(uid)
+        const category = await firebase.database().ref(`/users/${uid}/categories`).push({title,limit})
+        return {
+          title,
+          limit,
+          id:category.key}
+      }catch(e){
+        commit('setError', e)
+        throw e
+      }
+    }
+
+  }
+}
